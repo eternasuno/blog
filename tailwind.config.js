df6967b9c37fbc2431be8c3ca2d6f900cdee1e4a
huangliangshusheng <huangliangshusheng@gmail.com>
2021-05-15 17:24:33 +0800

    update code style

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,8 @@ module.exports = {
                         'blockquote p:first-of-type::before': false,
                         'blockquote p:last-of-type::after': false,
                         color: theme("colors.gray.700"),
-                        pre: {
-                            color: theme("colors.gray.700"),
-                            backgroundColor: "#FAF8F5",
+                        code: {
+                            color: theme("colors.gray.700")
                         },
                         "code::before": false,
                         "code::after": false,
@@ -52,9 +51,8 @@ module.exports = {
                             color: theme('colors.gray.300')
                         },
                         color: theme("colors.gray.300"),
-                        pre: {
-                            color: theme("colors.gray.300"),
-                            backgroundColor: "#1D262F",
+                        code: {
+                            color: theme("colors.gray.300")
                         },
                         h1: {
                             color: theme("colors.gray.100")
