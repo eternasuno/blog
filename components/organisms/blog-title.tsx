3d851f78f17b46c3155db85e0c19da1cc9d11140
huangliangshusheng <huangliangshusheng@gmail.com>
2021-05-18 14:05:00 +0800

    fix 404 page bug

--- components/organisms/blog-title.tsx
+++ components/organisms/blog-title.tsx
@@ -1,13 +1,18 @@
 import BLOG from "../../blog.config";
 
-const BlogTitle = () => {
+type Props = {
+    title?: string;
+    subTitle?: string;
+};
+
+const BlogTitle = ({ title = BLOG.title, subTitle = BLOG.description }: Props) => {
     return (
         <div className="space-y-2 md:space-y-5 pt-16 pb-20">
             <h1 className="font-extrabold tracking-tight capitalize text-3xl md:text-6xl">
-                {BLOG.title}
+                {title}
             </h1>
             <p className="text-gray-600">
-                {BLOG.description}
+                {subTitle}
             </p>
         </div>
     );
--- pages/404.tsx
+++ pages/404.tsx
@@ -2,21 +2,15 @@ import React from "react";
 import BlogTitle from "../components/organisms/blog-title";
 import Meta from "../components/organisms/meta";
 import Nav from "../components/organisms/nav";
-import StickyHeaderContent from "../components/templates/sticky-header-content";
+import BlogTemplate from "../components/templates/blog-template";
 
 const NotFound = () => (
     <>
         <Meta title="404 - Not Found" canonical="404" />
-        <StickyHeaderContent header={<Nav />} content={
-            <div className="divide-y divide-gray-200 dark:divide-gray-700">
-                <BlogTitle />
-                <div>
-                    <h2 className="mx-auto my-32 text-center font-bold capitalize text-2xl md:text-5xl">
-                        404 - Not Found
-                    </h2>
-                </div>
-            </div>
-        } />
+        <BlogTemplate nav={<Nav />}
+            header={<BlogTitle title="404 - Not Found" subTitle="Sorry, the page does not exist!" />}
+            content={<></>}
+        />
     </>
 );
 
