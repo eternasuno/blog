08bf152da288c4db4d2a270beea4bd4c6d051ae0
huangliangshusheng <huangliangshusheng@gmail.com>
2022-03-14 07:28:17 +0000

    update meta

--- components/organisms/meta.tsx
+++ components/organisms/meta.tsx
@@ -18,6 +18,7 @@ const Meta = ({ title, domain, canonical, description }: Props) => {
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
             />
+            <meta name="theme-color" content="#555" />
             <meta name="description" content={description} />
             <title>{title}</title>
 
