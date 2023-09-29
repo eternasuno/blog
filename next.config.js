5eef9b2557826cf61f3a2d2ab3c62eafbc6f86c9
eternasuno <eternasuno@icloud.com>
2023-09-29 06:52:23 +0000

    [feat] using @ducanh2912/next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
+    aggressiveFrontEndNavCaching: true,
+    cacheOnFrontEndNav: true,
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({});
+module.exports = withPWA({
+    reactStrictMode: true,
+});
